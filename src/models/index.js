8ae7a3d7ef2d517dcd97c88e288d275ecf2d765d
castberry10 <soc09072@gmail.com>
2024-07-30 16:01:44 +0900

    chore: Update sequelize configuration and import statements

--- src/models/index.js
+++ src/models/index.js
@@ -1,8 +1,10 @@
 // models/index.js
 // import { Sequelize, DataTypes } from 'sequelize';
-import { Sequelize } from 'sequelize';
+import { col, Sequelize } from 'sequelize';
 import Member from './member.js';
 import Gameification from './gamification.js';
+import dotenv from 'dotenv';
+dotenv.config();
 const sequelize = new Sequelize(
   process.env.MYSQL_DATABASE,
   process.env.MYSQL_USER,
@@ -10,8 +12,16 @@ const sequelize = new Sequelize(
   {
     host: process.env.MYSQL_HOST,
     dialect: 'mysql',
+    port: process.env.MYSQL_PORT,
+    // port: 3306,
   }
 );
+// console.log('sequelize: ', sequelize);
+console.log('process.env.MYSQL_DATABASE: ', process.env.MYSQL_DATABASE);
+console.log('process.env.MYSQL_USER: ', process.env.MYSQL_USER);
+console.log('process.env.MYSQL_PASSWORD: ', process.env.MYSQL_PASSWORD);
+console.log('process.env.MYSQL_HOST: ', process.env.MYSQL_HOST);
+console.log('process.env.MYSQL_PORT: ', process.env.MYSQL_PORT);
 
 const db = {};
 db.sequelize = sequelize;
