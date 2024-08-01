e8c035ea727783ebf282b6f58795a876d9afa45e
wychoi <soc09072@gmail.com>
2024-08-01 19:24:30 +0900

    chore: Update start script and add HTTPS support for production environment

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
-// import cookie from 'koa-cookie';
 import Koa from 'koa';
 import Router from 'koa-router';
 import api from './api/index.js';
 import bodyParser from 'koa-bodyparser';
 import db from './models/index.js';
-// const cors = require('@koa/cors');
 import cors from '@koa/cors';
-// import jwtMiddleware from './lib/jwtMiddleware.js';
+import https from 'https';
+import fs from 'fs';
 
-const { PORT } = process.env;
+const { PORT, NODE_ENV } = process.env;
 
 const app = new Koa();
 app.keys = [process.env.COOKIE_SECRET];
@@ -21,8 +20,6 @@ router.use('/api', api.routes()); // api 라우트
 
 // 라우터 적용 전 적용
 app.use(bodyParser());
-// app.use(jwtMiddleware);
-// app.use(cookie());
 db.sequelize.sync({ force: false })
     .then(() => {
         console.log('데이터베이스 연결됨.');
@@ -33,8 +30,21 @@ db.sequelize.sync({ force: false })
 // app 인스터스에 라우터 적용 
 app.use(router.routes()).use(router.allowedMethods());
 
-const port = PORT || 4000;
+const port = PORT || 1365;
 
-app.listen(port, () => {
-  console.log('Listening to port %d', port);
-});
+if (NODE_ENV === 'production') {
+  // 프로덕션 환경
+  const options = {
+    key: fs.readFileSync('/etc/letsencrypt/live/castberry.kr/privkey.pem'),
+    cert: fs.readFileSync('/etc/letsencrypt/live/castberry.kr/fullchain.pem')
+  };
+  
+  https.createServer(options, app.callback()).listen(port, () => {
+    console.log('Listening to port %d with HTTPS', port);
+  });
+} else {
+  // 개발 환경
+  app.listen(port, () => {
+    console.log('Listening to port %d', port);
+  });
+}
